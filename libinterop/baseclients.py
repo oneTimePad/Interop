4a883f949e11b22d5eed39910a8c702579a5ea46
Dylan Herman <dah242@scarletmail.rutgers.edu>
2017-02-27 17:13:12 +0000

    added extensive documentation

---
+++ libinterop/baseclients.py
@@ -0,0 +1,109 @@
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import json
+import base64
+import pdb
+import sys
+import time
+from types import Mission,StationaryObstacle,MovingObstacle
+"""
+	These classes define the "base" clients used to contact
+	the interop proxy server (Imaging GCS django).
+"""
+
+
+STATUS_OK = 200
+
+workers = 5
+
+class _Client(object):
+	"""
+		synchronous client for the imaging proxy server
+	"""
+
+	def __init__(self,url,username,password,timeout=5):
+		self.url = url
+		self.timeout = timeout
+		self.username = username
+		self.password = password
+		self.login()
+
+
+	def parse_token(self,resp):
+		"""
+			the Json-web token returned by the server is base64 encoded
+			we need to parse the token and its expiration
+			arg := b64 encoded token
+		"""
+		token_resp = resp.json()
+		token = token_resp['token']
+		exp64 = token+"="*(4-len(token)%4)
+		decoded = exp64.decode("base64")
+		self.exp =json.loads(decoded.partition('}')[2].partition('}')[0]+"}")['exp']
+		self.token = token_resp
+
+
+	def login(self):
+		global STATUS_OK
+		"""
+			logs into the Imaging GCS
+		"""
+		resp = requests.post(self.url+'/interop/login',headers={'Content-Type':'application/json; charset=UTF-8'},data= json.dumps({'password':self.password,'username':self.username}))
+
+		if resp.status_code == STATUS_OK:
+			self.parse_token(resp)
+		else:
+			raise Exception(resp.status_code)
+
+	def post_telemetry(self,data):
+		"""
+			proxy method for posting telemetry to the interop server
+			args := Telemetry object
+			returns := tuple(time of response,error)
+		"""
+		resp =requests.post(self.url+'/interop/postTelemetry',data=data.serialize(), headers={'Authorization':'JWT '+self.token['token']})
+		return (resp.json()['time'],resp.json()['error'])
+
+	def get_mission(self):
+		"""
+			proxy method for fetching missions and returns them as a list in mission object form
+			returns := list( Misssion_obj , ... )
+		"""
+		resp = requests.post(self.url+'/interop/getMission',headers={'Authorization':'JWT '+self.token['token']})
+		return ([Mission(**resp.json()[key]) for key in resp.json().keys() if key !='error'],resp.json()['error'])
+	def get_obstacles(self):
+		"""
+			proxy method for fetching moving and stationary obstacles
+			returns := tuple( list(moving_obstacle,...), list(stationary_obstacle,..), error )
+		"""
+
+		resp = requests.post(self.url+'/interop/getObstacles',headers={'Authorization':'JWT '+self.token['token']})
+
+		return ([MovingObstacle(**resp.json()['moving'][key]) for key in resp.json()['moving'].keys()],[StationaryObstacle(**resp.json()['stationary'][key]) for key in resp.json()['stationary'].keys()],resp.json()['error'])
+
+class AsyncClient(object):
+	"""
+		future "enhanced" client for providing pipeling and concurrency
+	"""
+	def __init__(self,url,username,password):
+		"""
+			creates the Thread pool executor and logs in 
+		"""
+
+		self.executor = ThreadPoolExecutor(max_workers = workers)
+		while True:
+			try:
+				self.client=_Client(url,username,password)
+				break
+			# due to a weird token formatting error, we need to keep logging in untill a good token is returned
+			except(TypeError,binascii.Error):
+				continue #continue to attempt upload on token decode failure
+
+	def post_telemetry(self,data):
+		return self.executor.submit(self.client.post_telemetry,data)
+
+	def get_mission(self):
+		return self.executor.submit(self.client.get_mission)
+
+	def get_obstacles(self):
+		return self.executor.submit(self.client.get_obstacles)
--- libinterop/interopclients.py
+++ libinterop/interopclients.py
@@ -1,4 +1,4 @@
-from client import InteropClient
+from baseclient import AsyncClient
 from types import Telemetry
 import binascii
 import time
@@ -6,19 +6,37 @@ import threading
 import dronekit
 import datetime
 
-class HandlerInteropClient(InteropClient):
+
+"""
+	Clients that implement the actual functionality specific to each interop task
+"""
+
+class InteropClient(AsyncClient):
+	"""
+		Implements certain functionality that is useful through some of the interop tasks
+		such as debugging and registering handlers for the clients that use futures
+		for piplining
+	"""
 
 	def __init__(self,proxy_info,poll_info,callback=None,status_debug = True):
+		"""
+			args := poll_info = { POLL_SEC , PRINT_SEC}  (dictionary containng poll time and print time)
+					proxy_info =  { host, port, username, password} (all for proxy interop server (imaging server)
+
+		"""
 		if not all(key in proxy_info for key in("username","password","host","port")):
 			raise Exception("HandlerInteropClient<Bad argument>: prox_info = \
 				   dict('username','password','host','port')")
-
+		#register a callback to be executed when client receives response from server
 		self.future_callback = callback
+		#whether debugging is enable (prints debug statements)
 		self.status_debug = status_debug
 		if status_debug:
 
 			self.status_lock = threading.Lock()
+			#number of requests sent since we last printed a debug statement
 			self.sent_since_print = 0
+			#use now as the start time
 			self.last_print = datetime.datetime.now()
 		self.POLL_SEC = poll_info["poll_sec"]
 		self.PRINT_SEC = poll_info["print_sec"] if ("print_sec" in poll_info)else 0
@@ -27,7 +45,10 @@ class HandlerInteropClient(InteropClient):
 		super(HandlerInteropClient,self).__init__("http://"+proxy_info["host"]+":"+proxy_info["port"],proxy_info["username"],proxy_info["password"])
 
 	def handler_contact_callback(self,future):
-
+		"""
+			default callback registered for all clients using futures
+			prints debug statments, check for errors and calls the other callback registered in the arg to __init__
+		"""
 		if future.exception():
 			print("WARNING: Request Failed with %s" % (str(future.exception())))
 		else:
@@ -36,7 +57,7 @@ class HandlerInteropClient(InteropClient):
 			
 				with self.status_lock:
 					self.sent_since_print+=1
-
+			# if the client has a registered callback passed in the constructor, call it now
 			if self.future_callback:
 				try:
 					self.future_callback(*future.result())
@@ -44,28 +65,40 @@ class HandlerInteropClient(InteropClient):
 					print("WARNING: Registered callback Failed with %s" % (str(e)))
 		
 	def _make_request(self):
+		"""
+			the actual request method that must be overriden by subclasses
+		"""
 		raise NotImplementedError("_make_request must be overridden")
 	
 	def start(self,THREADING_ENABLED=True):
-
+		"""
+			starts up the requesting and multithreads if needed
+			args := THREADING_ENABLED (optional should the requesting be ran in a separate thread)
+		"""
+		#call synchronously
 		if not THREADING_ENABLED:
 			self._start()
 		else:
+			#thread and deamonize
 			self.thread = threading.Thread(target=self._start)
 			self.thread.daemon = True
 			self.thread.start()
 
 
 	def _start(self):
+		"""
+			starts the requesting process
+		"""
 		while True:
+			# try to call the subclasses request method and add the callback to the future
 			try:
 				self._make_request().add_done_callback(self.handler_contact_callback)
 			except Exception as e:
-				raise Exception("WARNING: Request for Object Failed with %s" %
-	 (str(e)))
+				raise Exception("WARNING: Request for Object Failed with %s" % (str(e)))
 			if not self.status_debug:
 				pass
 			else:
+				#if we are debugging, print debug statments and times
 				now = datetime.datetime.now()
 				since_print = (now-self.last_print).total_seconds()
 
@@ -73,50 +106,66 @@ class HandlerInteropClient(InteropClient):
 					with self.status_lock:
 						local_sent_since_print = self.sent_since_print
 						self.sent_since_print = 0
-						print("DEBUG: Upload Rate %f"
-	  %(local_sent_since_print/since_print))
+						print("DEBUG: Upload Rate %f" %(local_sent_since_print/since_print))
 						self.last_print = now
+			#attempt to achieve the request request rate
 			time.sleep(self.POLL_SEC)
 
 
 
 
-class TelemetryInterop(HandlerInteropClient):
+class TelemetryInterop(InteropClient):
+	"""
+			posting UAV current telemetry interop task
+
+	"""
 	def __init__(self,proxy_info,poll_info,mav_info,status_debug=True):
 		if not all(key in mav_info for key in ("host","port")):
 			raise Exception("TelemtryInterop required mav_info=dict('host','port') to contact mavproxy")
+		#connects to MAVProxy via Dronekit over udp
 		self.mav_endpoint = mav_info["host"] +":"+mav_info["port"]
 		
 		try:
-			
+			#make the dronekit connection
 			self.drone = dronekit.connect(self.mav_endpoint,wait_ready=True)
 		except Exception as e:
-			print("TelemetryInterop failed to setup mav connection, Failed with\
-		 %s" %(str(e)))
+			print("TelemetryInterop failed to setup mav connection, Failed with %s" %(str(e)))
 			return
-		
+		#create an initial telemetry
 		self.last_telemetry = Telemetry(0,0,0,0)
 
 		super(TelemetryInterop,self).__init__(proxy_info,poll_info,status_debug=status_debug)
 
 	def _make_request(self):
+		"""
+			post the current telemetry
+		"""
 		drone = self.drone
-		#TODO : make data accurate
+		# fetch the current telemetry from dronekit (lat,lon,altitude MSL, heading)
 		telemetry = Telemetry(latitude=float(drone.location.global_frame.lat),
 					   longitude=float(drone.location.global_frame.lon),
 					   altitude_msl=float(drone.location.global_frame.alt),
 					   uas_heading = 0)
+		#only send if telemtry has changed
 		if telemetry != self.last_telemetry:
 			return self.post_telemetry(telemetry)
 
 
-class ObstacleInterop(HandlerInteropClient):
-	
+class ObstacleInterop(InteropClient):
+	"""
+		requesting obstacle locations for SDA
+
+	"""
 	def _make_request(self):
 		return self.get_obstacles()
 
-class MissionInterop(InteropClient):
+class MissionInterop(AsyncClient):
+
+	"""
+		request the mission
+	"""
 	def __init__(self,proxy_info):
+		#no need for callbacks or looping requests (only need to grab the mission once)
 		super(MissionInterop,self).__init__("http://"+proxy_info["host"]+":"+proxy_info["port"],
 									  proxy_info["username"],
 									  proxy_info["password"])
